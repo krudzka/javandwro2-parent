f63a4638149cd4622a83d6a9fbeced99c9a59388
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-11 12:24:13 +0100

    update

--- io/src/main/java/pl/jwrabel/trainings/javandwro2/FileStreams.java
+++ io/src/main/java/pl/jwrabel/trainings/javandwro2/FileStreams.java
@@ -1,8 +1,9 @@
 package pl.jwrabel.trainings.javandwro2;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by jakubwrabel on 11.03.2017.
@@ -35,13 +36,107 @@ public class FileStreams {
 //			System.out.println((char) read);
 //		}
 
-		byte[] array = new byte[4];
-		fileInputStream.read(array);
-		String s = new String(array);
-		System.out.println(s);
+//		byte[] array = new byte[4];
+//		fileInputStream.read(array);
+//		String s = new String(array);
+//		System.out.println(s);
+//
+//		fileInputStream.close();
+//
 
-		fileInputStream.close();
+		// WCZYTYWANIE CYFRY Z PLIKU
+//		FileInputStream fileInputStream1 = new FileInputStream("testLiczba.txt");
+//
+//		int value = fileInputStream1.read() - 48;
+//		System.out.println(value);
+//
+//		fileInputStream1.close();
 
+		// WCZYTYWANIE LICZBY Z PLIKU
+//		FileInputStream fis = new FileInputStream("C:\\Users\\RENT\\Desktop\\testLiczba.txt");
+		FileInputStream fis = new FileInputStream("testLiczba.txt");
+
+		List<Integer> list = new ArrayList<>();
+		int readValue;
+		while ((readValue = fis.read()) > 0) {
+			int numberValue = readValue - 48;
+			list.add(numberValue);
+		}
+
+		int number = 0;
+		int multiplier = 1;
+		for (int i = list.size() - 1; i >= 0; i--) {
+			number += list.get(i) * multiplier;
+			multiplier *= 10;
+//			multiplier = multiplier*10;
+		}
+
+		System.out.println(number);
+
+
+		// SCIEŻKA WZGLĘDNA I BEZWZGLĘDNA
+		// BEZWZGLĘDNA (ABSOLUTE PATH) C:\test\mojFolder\test.txt
+		// WZGLEDNA (RELATIVE PATH)   mojFolder\test.txt
+
+		// PRZECZYTANIE CAŁEGO PLIKU DO STRINGA
+//		FileInputStream fisLoremIpsum = new FileInputStream("testLoremIpsum.txt");
+//		StringBuilder stringBuilder = new StringBuilder();
+//
+//		int tmp;
+//		while ((tmp = fisLoremIpsum.read()) > -1) {
+////			if ((char) tmp != '\n') {
+////				stringBuilder.append((char) tmp);
+////			}
+//			stringBuilder.append((char) tmp);
+//		}
+//
+////		System.out.println(stringBuilder);
+////		System.out.println(stringBuilder.toString().replaceAll("\n", ""));
+//		System.out.println(stringBuilder.toString());
+
+
+		// BufferedInputStream
+		FileInputStream inputStreamToBuffer = new FileInputStream("testLoremIpsum.txt");
+		BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStreamToBuffer);
+
+//		bufferedInputStream.read()
+
+
+		bufferedInputStream.close();
+		inputStreamToBuffer.close();
+
+		// BufferedOutputStream
+		FileOutputStream fosToBuffer = new FileOutputStream("test.txt");
+		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fosToBuffer);
+
+		bufferedOutputStream.write(97);
+
+		bufferedOutputStream.flush();
+		bufferedOutputStream.close();
+		fosToBuffer.close();
+
+
+		// System.console()
+		System.console().readLine();
+		new Scanner(System.in).nextLine();
+
+
+		// FileWriter
+		FileWriter fileWriter = new FileWriter("test.txt");
+//		FileWriter fileWriter = new FileWriter("test.txt", true);
+		fileWriter.write("asdfghj");
+		fileWriter.close();
+
+		// FileReader
+		FileReader fileReader = new FileReader("test.txt");
+		int read = fileReader.read();
+		fileReader.close();
+
+		// BufferedWriter
+		FileWriter out = new FileWriter("test.txt");
+		BufferedWriter bufferedWriter = new BufferedWriter(out);
+
+		// BufferedReader
 
 	}
 }
